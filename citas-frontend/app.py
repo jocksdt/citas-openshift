eab21c70ddc576b47dcc38115dbe1c9358cf3578
José Domingo Muñoz <josedom24@gmail.com>
2023-03-20 19:53:28 +0100

    citas-frontend

---
+++ citas-frontend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)	
+
+
+@app.route('/',methods=["GET"])
+def inicio():
+    noserver=False
+    lista=[]
+    
+    try:
+        server=os.environ["CITAS_SERVER"]
+        url="http://"+server+"/quotes/random"
+        r=requests.get(url, timeout=1)
+        datos=r.json()
+    except:
+        noserver=True
+ 
+    return render_template("inicio.html",noserver=noserver,datos=datos)
+
+
+app.run('0.0.0.0',5000,debug=True)
