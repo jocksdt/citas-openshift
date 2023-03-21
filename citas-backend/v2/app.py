f5c83183b06302dd9766d92d653ec517442989d0
José Domingo Muñoz <josedom24@gmail.com>
2023-03-21 14:07:33 +0100

    new v2

--- citas-backend/v2/app.py
+++ citas-backend/v2/app.py
@@ -34,35 +34,19 @@ def health():
 
 @app.route('/quotes', methods=['GET'])
 def getQuotes():
-    global quotes
+    quotes = getQuotes()
     return prepareResponse(jsonify(quotes))
   
 @app.route('/quotes/<int:id>', methods=['GET'])
 def getQuoteById(id):
-    return prepareResponse(jsonify(replaceHostname(quotes[id])))
+    return prepareResponse(jsonify(replaceHostname(getQuotes()[id])))
 
 @app.route('/quotes/random', methods=['GET'])
 def getRandom():
-    try:
-        conn = MySQLdb.connect(
-            user=os.environ['USER_DB'],
-            password=os.environ['PASSWORD_DB'],
-            host=os.environ['HOST_DB'],
-            database="citas",
-            port=3306)
-        
-        mycursor = conn.cursor()
-        mycursor.execute("SELECT '-hostname-' as hostname, id, quotation, author FROM quotes ORDER BY author, id")
-        
-        rows = mycursor.fetchall()
-        n = random.randint(0,(mycursor.rowcount)-1)
-        conn.close()
-        quotes = rows
-        return prepareResponse(jsonify(replaceHostname(quotes[n])))
-    except:
-        print(f"Error connecting to MariaDB Platform:")
-        sys.exit(1)
-
+    tquotes = getQuotes()
+    n = random.randint(0,len(quotes)-1)
+    return prepareResponse(jsonify(replaceHostname(quotes[n])))
+    
 def prepareResponse(response):
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -73,8 +57,8 @@ def replaceHostname(jsondoc):
     q = q.replace('-hostname-', socket.gethostname())
     return json.loads(q)
 
-def main():
-    global quotes
+def getQuotes():
+    quotes=[]
     try:
         conn = MySQLdb.connect(
             user=os.environ['USER_DB'],
@@ -85,9 +69,10 @@ def main():
         mycursor = conn.cursor()
         mycursor.execute("SELECT '-hostname-' as hostname, id, quotation, author FROM quotes ORDER BY author, id")
         quotes = mycursor.fetchall()
-        
+        conn.close()
     except:
         sys.exit(1)
+    return quotes
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000)
